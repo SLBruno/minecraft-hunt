3936985f2f05da0c3584b6c55a9556baf9889d5b
slbruno <brunolagoela@gmail.com>
2016-08-03 17:20:16 -0300

    tradução de alertas do devise

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -8,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Mhunt03
   class Application < Rails::Application
       config.active_record.observers = :video_observer
+      config.i18n.default_locale = :'pt-BR'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
